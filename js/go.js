cf91fb4ecf6f706c63ca451954bcd2888525c481
Kelsey Dawes <kdawes@gmail.com>
2015-09-18 23:26:43 -0600

    standardjsification

--- js/go.js
+++ js/go.js
@@ -1,64 +1,62 @@
-      var Proc = require ('./Proc')
-      var shannon = require ('./shannon')
-      var engine = new Proc ()
-      var director = require ('director')
+var Proc = require('./Proc')
+var shannon = require('./shannon')
+var engine = new Proc()
+var director = require('director')
 
-      var routes = {
-        '/filter': filter,
-        '/shannon': shannon,
-        '/raw': raw,
-        '/list': list,
-        "/":list
-      }
+var routes = {
+  '/filter': filter,
+  '/shannon': shannon,
+  '/raw': raw,
+  '/list': list,
+  '/': list
+}
 
-      var config = {
-        "engine": {
-        },
-        "width":16,
-        "height":16,
-        "spanw":2048,
-        "spanh":16384,
-        "grid":false
-      }
+var config = {
+  'engine': {
+  },
+  'width': 16,
+  'height': 16,
+  'spanw': 2048,
+  'spanh': 16384,
+  'grid': false
+}
 
-      function shannon () {
-        console.log ("SHannon route")
-        var cfg = _.cloneDeep (config)
-        cfg.engine.blksz = 20
-        cfg.engine.type = "shannon"
-        engine.go (cfg)
-      }
+function shannon () {
+  console.log('SHannon route')
+  var cfg = _.cloneDeep(config)
+  cfg.engine.blksz = 20
+  cfg.engine.type = 'shannon'
+  engine.go(cfg)
+}
 
-      function filter () {
-        console.log ("Filter route")
-        var cfg = _.cloneDeep (config)
-        cfg.engine.type = "filter"
-        cfg.width=16
-        cfg.height=16
-        cfg.grid = true
-        engine.go (cfg)
-      }
+function filter () {
+  console.log('Filter route')
+  var cfg = _.cloneDeep(config)
+  cfg.engine.type = 'filter'
+  cfg.width = 16
+  cfg.height = 16
+  cfg.grid = true
+  engine.go(cfg)
+}
 
-      function raw () {
-        console.log ("raw route")
-        var cfg = _.cloneDeep (config)
-        cfg.engine.type = "raw"
-        cfg.grid = true
-        engine.go (cfg)
-      }
+function raw () {
+  console.log('raw route')
+  var cfg = _.cloneDeep(config)
+  cfg.engine.type = 'raw'
+  cfg.grid = true
+  engine.go(cfg)
+}
 
-      function list () {
-        $ ().ready (function (){
-          $ ("#msgs").empty ()
-          Object.keys (routes).forEach (function (r) {
-            var ctnt = "<a href='/#" + r +"'>"+ r +"</a>"
-            console.log ("LIST :> " + ctnt)
+function list () {
+  $().ready(function () {
+    $('#msgs').empty()
+    Object.keys(routes).forEach(function (r) {
+      var ctnt = '<a href=/#' + r + '>' + r + '</a>'
+      console.log('LIST :> ' + ctnt)
+      $('#msgs').append(ctnt + ' </br>')
+    })
+  })
+}
 
-            $ ("#msgs").append (ctnt + " </br>")
-
-          })
-        })
-      }
-
-       var router = new director.Router (routes)
-      router.init ()
+var router = new director.Router(routes)
+router.init()
