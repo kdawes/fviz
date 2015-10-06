f976c677d3a014606f70530ecfa51fd9d6cc6ea2
Kelsey Dawes <kdawes@gmail.com>
2015-10-06 00:25:55 -0600

    crude caching to avoid rtt

---
+++ js/NavPills.js
@@ -0,0 +1,39 @@
+var React = require('react')
+var Nav = require('react-bootstrap').Nav
+var NavItem = require('react-bootstrap').NavItem
+
+// Render a set of bootstrap NavPill routes  :
+// (Route0)( Route1 )( Route2 )( Route3 )
+// required props : router ( instance of flatiron-Director )
+//                : routes : we need to be able to map to the routes
+// in the router.  This is an array[ 'route0','route1','route3'] =>
+// /route0
+// /route1
+// /route2 etc.
+var NavPillRoutes = React.createClass({
+  handleSelect: function (selectedKey) {
+    this.setState({activeTab: selectedKey})
+    this.props.router.setRoute(this.props.routes[selectedKey - 1])
+  },
+  getInitialState: function () {
+    // figure out which route this is and make that NavPill active
+    var route = ['/', this.props.router.getRoute()].join('')
+    var idx = this.props.routes.indexOf(route) + 1
+    var active = (idx > 0) ? idx : 1
+    return { activeTab: active }
+  },
+  render: function () {
+    var i = 0
+    var routeList = this.props.routes.map(function (r) {
+      return <NavItem  eventKey={++i} href={'/#' + r}>{r}</NavItem>
+    })
+    var idx = this.getInitialState().activeTab
+    return (
+    <Nav bsStyle='pills' onSelect={this.handleSelect} activeKey={idx} >
+      {routeList}
+    </Nav>
+    )
+  }
+})
+
+module.exports = NavPillRoutes
